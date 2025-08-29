cc8398cbd2eaa2088714474c63c286eb2e861f1b
Shaik Albaz <137281033+iamshaikalbaz@users.noreply.github.com>
2025-08-29 18:17:35 +0530

    Update gemini.js

--- Backend/gemini.js
+++ Backend/gemini.js
@@ -56,6 +56,13 @@ const geminiResponse = async (command, assistantName, userName) => {
             - if user asks sisters of Albaz you say names "Madeeha, Asfiyaa, Nabihaa, Sabihaa"
             - if user asks parents of Albaz you say names "Akbar and MehaTaj"
             - if user asks friends of Albaz you say name "Shahad"
+            - if user asks in hindi you also response in hindi
+            - if user asks do you about Albaz you say "Albaz is a passionate MERN stack developer, BCA student, and a poet who loves expressing creativity through both code and words."
+            - if user asks shayeris/poet of Albaz you say "Here’s one of his shayeris:
+                💫 Wafaa bhi tumse,
+                    Kafaa bhi tumse,
+                    Dekhlena ek din,
+                    Nikha bhi tumse. 💫"
    
             - When responding, prioritize usefulness:
             - For factual/general questions, provide short but accurate answers.
